41ae7f8ed8a2193c4af04c1cab4cfb3ae4f8fdba
naz <naz@sakib>
2025-12-14 23:42:21 +0100

    api changed

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // config.js
 const CONFIG = {
-  BASE_URL: "https://localhost:9090"
+  //BASE_URL: "https://localhost:9090"
+  BASE_URL: "https://thallous-pellucidly-delilah.ngrok-free.dev"
 };
 
 export default CONFIG;
